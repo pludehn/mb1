a8c7ddbb37ac08935049a08cc3a630f940b3a81f
Bowtie <ian@iangolden.com>
2017-05-20 19:09:21 -0600

    Saving WIP

---
+++ Map_Board/Fixture.swift
@@ -0,0 +1,42 @@
+//
+//  Fixture.swift
+//  Map_Board
+//
+//  Created by Paul Udehn on 5/20/17.
+//  Copyright © 2017 Paul Udehn. All rights reserved.
+//
+
+import SpriteKit
+class Fixture: SKSpriteNode {
+    var lastPosX: CGFloat!
+    var lastPosY: CGFloat!
+
+    init(fixtureWidth: Double, fixtureHeight: Double) {
+        let texture = SKTexture(imageNamed: "fixture1")
+        let red = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        super.init(texture: texture, color: red, size: CGSize(width: 80, height: 160))
+        self.name = "myNodeName"
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let node : SKNode = self.atPoint(location)
+            if node.name == "myNodeName" {
+                self.lastPosX = self.position.x
+                self.lastPosY = self.position.y
+            }
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let node : SKNode = self.atPoint(location)
+            if node.name == "myNodeName" {
+                self.position.x = touch.location(in: nil).x
+            }
+        }
+    }
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
